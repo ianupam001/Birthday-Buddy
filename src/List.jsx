2d352bb7c55b571f70f4c7bee342ed7408030a25
Anupam Tiwari <anupamtiwari8090@gmail.com>
2024-01-14 16:37:12 +0530

    First Commit

---
+++ src/List.jsx
@@ -0,0 +1,10 @@
+import { Person } from "./Person";
+export const List = ({people}) => {
+  return (
+    <section>
+      {people.map((person) => {
+        return <Person key={person.id} {...person} />;
+      })}
+    </section>
+  );
+};
